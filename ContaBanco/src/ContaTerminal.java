92ac3de1fb6be6400b301c0cd986a8ea056b0da1
Ulpio <ulpionetto0@gmail.com>
2022-09-28 11:31:17 -0300

    try scanner

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -5,18 +5,17 @@ public class ContaTerminal {
         int numero;
         String agencia,nomeCliente;
         double saldo;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Olá, Bem vindo ao banco Úlpio\nPor favor insira o Número da conta.");
-        numero = sc.nextInt();
-        System.out.println("Por favor insira o número da Agência:");
-        agencia = sc.nextLine();
-        agencia = sc.nextLine();
-        System.out.println("Insira o seu Nome:");
-        nomeCliente = sc.nextLine();
-        System.out.println("Insira o seu Saldo:");
-        saldo = sc.nextDouble();
-
-
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Olá, Bem vindo ao banco Úlpio\nPor favor insira o Número da conta.");
+            numero = sc.nextInt();
+            System.out.println("Por favor insira o número da Agência:");
+            agencia = sc.nextLine();
+            agencia = sc.nextLine();
+            System.out.println("Insira o seu Nome:");
+            nomeCliente = sc.nextLine();
+            System.out.println("Insira o seu Saldo:");
+            saldo = sc.nextDouble();
+        }
         System.out.println("Um momento, carregando...\n");
         System.out.println("Olá"+nomeCliente+"Obrigado por criar uma conta em nosso banco.");
         System.out.println("Sua agência é: "+agencia+" conta: "+numero+" e seu saldo: "+saldo+" Já está disponivel para saque.");
